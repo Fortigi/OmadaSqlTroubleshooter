0e3695f330503c6c2a19ab55a305623f2e9db316
mvaneijken <mark@fortigi.nl>
2026-07-15 19:51:00 +0200

    Fix gallery version and module info lookup  (#35)
    * Fix gallery version lookup
    
    * Fix Get-InstalledModuleInfo
    
    * Remove -UseBasicParsing it is not functional for PowerShell Core.

---
+++ tests/Get-GalleryModuleVersion.Tests.ps1
@@ -0,0 +1,53 @@
+BeforeAll {
+    $ParentPath = Split-Path -Path $PSScriptRoot -Parent
+    $Command = Join-Path $ParentPath -ChildPath "src\lib\functions\Private\Get-GalleryModuleVersion.ps1"
+    . $Command
+    $Script:Tracer = [System.Diagnostics.Trace]
+    $Script:RunTimeConfig = [PSCustomObject]@{ ApplicationName = "Test" }
+}
+
+Describe 'Get-GalleryModuleVersion' -Tag 'Unit' {
+    It 'Should return the version of the most recently updated package' {
+        Mock Invoke-RestMethod {
+            @(
+                [PSCustomObject]@{
+                    Properties = [PSCustomObject]@{
+                        version   = '1.0.0'
+                        Published = [PSCustomObject]@{ '#text' = (Get-Date).AddDays(-5) }
+                    }
+                }
+                [PSCustomObject]@{
+                    Properties = [PSCustomObject]@{
+                        version   = '2.0.0'
+                        Published = [PSCustomObject]@{ '#text' = (Get-Date) }
+                    }
+                }
+            )
+        }
+
+        Get-GalleryModuleVersion -ModuleName 'OmadaWeb.PS' | Should -Be '2.0.0'
+    }
+
+    It 'Should return $null when the gallery response is empty' {
+        Mock Invoke-RestMethod { $null }
+
+        Get-GalleryModuleVersion -ModuleName 'OmadaWeb.PS' | Should -BeNullOrEmpty
+    }
+
+    It 'Should return $null when the request fails' {
+        Mock Invoke-RestMethod { throw 'network error' }
+
+        Get-GalleryModuleVersion -ModuleName 'OmadaWeb.PS' | Should -BeNullOrEmpty
+    }
+
+    It 'Should request the correct PowerShell Gallery API endpoint' {
+        Mock Invoke-RestMethod { $null }
+
+        Get-GalleryModuleVersion -ModuleName 'OmadaWeb.PS' | Out-Null
+
+        Should -Invoke Invoke-RestMethod -ParameterFilter {
+            $Uri -eq "https://www.powershellgallery.com/api/v2/FindPackagesById()?id='OmadaWeb.PS'"
+        }
+    }
+}
+
